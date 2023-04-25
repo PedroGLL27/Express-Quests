22d008a2818a2028d7383bfbbff1afeeb96330e4
PedroGLL27 <pedrolou27@gmail.com>
2023-04-25 17:13:40 +0100

    express02

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
+require('dotenv').config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5000;
+
+
+
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to the users list");
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+//const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+//app.get("/api/movies", movieHandlers.getMovies);
+//app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
+
+app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.putUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
@@ -22,3 +37,8 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
